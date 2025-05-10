20a55ce4276b03d0910bbc2664951903daf7fb95
Erkan3034 <turguterkan55@gmail.com>
2025-05-10 23:58:32 +0300

    Enhance ToDo application: fetch and display todos in index, add delete and complete functionality

--- app.py
+++ app.py
@@ -5,10 +5,19 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 db = SQLAlchemy(app)
 
+
+class ToDo(db.Model):
+    __tablename__ = 'todo'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    complete = db.Column(db.Boolean)
+
+
 #Main page
 @app.route('/')
 def index():
-    return render_template('index.html')
+    todos = ToDo.query.all() # Fetch all todos from the database
+    return render_template('index.html', todos=todos)
 
 
 @app.route('/add', methods=['POST'])
@@ -20,11 +29,22 @@ def addToDo():
     db.session.commit()
 
     return redirect(url_for('index'))
-class ToDo(db.Model):
-    __tablename__ = 'todo'
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80), nullable=False)
-    complete = db.Column(db.Boolean)
+
+
+@app.route('/delete/<int:todo_id>')
+def deleteToDo(todo_id):
+    todo = ToDo.query.get(todo_id)
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/complete/<int:todo_id>')
+def completeToDo(todo_id):
+    todo = ToDo.query.get(todo_id)
+    todo.complete = not todo.complete # Toggle the complete status
+    db.session.commit()
+    return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     with app.app_context():
