5e2832b37125ebbf436acadc48a675d4a54adbc1
Erkan3034 <turguterkan55@gmail.com>
2025-05-10 23:21:37 +0300

    Add ToDo management functionality and create index template

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, url_for,redirect, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 db = SQLAlchemy(app)
 
+#Main page
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/add', methods=['POST'])
+def addToDo():
+    title = request.form.get('title')
+    #complete = request.form.get('complete')
+    new_todo = ToDo(title=title,complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+
+    return redirect(url_for('index'))
 class ToDo(db.Model):
     __tablename__ = 'todo'
     id = db.Column(db.Integer, primary_key=True)
